1835f838440c261d2fe7ff29bfeac846c772f69d
Michael Ellison <mcellison3131@gmail.com>
2020-07-29 22:30:42 -0400

    added Birthday to my App

--- michael/urls.py
+++ michael/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("birthday", views.birthday, name="birthday")
 ]
 
--- michael/views.py
+++ michael/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'michael/index.html')
+
+
+def birthday(request):
+    today = datetime.datetime.now()
+    return render(request, "michael/birthday.html", {
+        "birthday": (today.day == 22) & (today.month == 7)
+    })
